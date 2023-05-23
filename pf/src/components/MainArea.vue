57742806a11e5dc850ec8f23823297c50d06f07b
kdy <dy.kim@hkcloud.co.kr>
2023-05-23 17:43:31 +0900

    media 1235

--- pf/src/components/MainArea.vue
+++ pf/src/components/MainArea.vue
@@ -1,36 +1,79 @@
 <template>
-  <div class="mainContent">
-    <div class="mainTxtBx">
-      <span class="mainTxt">Kdy2da</span>
-      <span class="mainSubtxt">안녕하십니까, 여긴 제 포트폴리오 공간입니다.</span>
-      <span class="mainSubtxt">UI/UX Design 및 Publishing을 하고 있습니다. 그럼 enjoy</span>
-    </div>
-    <div class="mainBtnBx">
-      <div class="mainBtns">
-        <div class="mainBtn">All</div>
-        <div class="mainBtn">UI/UX Design</div>
-        <div class="mainBtn">Project</div>
-        <div class="mainBtn">Work</div>
+    <div class="mainContent">
+      <div class="mainTxtBx">
+        <span class="mainTxt">Kdy2da</span>
+        <!-- <span class="mainSubtxt">안녕하십니까, 여긴 제 포트폴리오 공간입니다.</span> -->
+        <!-- <span class="mainSubtxt">UI/UX Design 및 Publishing을 하고 있습니다. 그럼 enjoy</span> -->
       </div>
-      <button class="btn-secondary like-review"  @click="likeBtn">
-        <i class="fa fa-heart" aria-hidden="true" ref="myButton" :class="{ animateLike: isActive }"></i>{{likeNum}}
-      </button>
+      <div>
+        <div class="mainBtnBx">
+
+          <div class="mainBtns">
+            <div class="mainBtn" @click="toggleBComponent(null)" :class="{ isActive: selectedId === null && selectedBtn }">All</div>
+            <div class="mainBtn" @click="toggleBComponent(1)" :class="{ isActive: selectedId === 1 && selectedBtn }">UI/UX Design</div>
+            <div class="mainBtn" @click="toggleBComponent(2)" :class="{ isActive: selectedId === 2 && selectedBtn }">Project</div>
+            <div class="mainBtn" @click="toggleBComponent(3)" :class="{ isActive: selectedId === 3 && selectedBtn }">Work</div>
+          </div>
+
+          <button class="btn-secondary like-review"  @click="likeBtn">
+            <i class="fa fa-heart" aria-hidden="true" ref="myButton" :class="{ animateLike: isActive }"></i>{{likeNum}}
+          </button>
+
+        </div>
+      </div>
     </div>
-  </div>
+  <WorkArea v-if="show" ref="workArea" :id="selectedId"></WorkArea>
 </template>
 
 <script>
+import WorkArea from '../components/WorkArea.vue'
+
 export default {
   name: 'MainArea',
-  props: {},
-  components: {},
+  props: {
+
+  },
+  components: {
+    WorkArea,
+  },
   data() {
     return{
       likeNum: 0,
       isActive: false,
+      show: false,
+      selectedId: undefined,
+      selectedBtn: false,
+    }
+  },
+  created() {
+    // 페이지가 로드될 때, 로컬 스토리지에서 likeNum 값을 가져옴
+    const savedLikeNum = localStorage.getItem('likeNum');
+    if (savedLikeNum) {
+      this.likeNum = parseInt(savedLikeNum, 10);
     }
   },
   methods: {
+    // workareashow
+    toggleBComponent(id) {
+      this.show = true;
+      this.selectedId = id;
+      this.selectedBtn = true;
+
+      this.$nextTick(() => {
+        if (this.show) {
+          this.scrollToWorkArea();
+        }
+      });
+    },
+    scrollToWorkArea() {
+      const workArea = this.$refs.workArea;
+      if (workArea) {
+        workArea.$el.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    },
     likeBtn() {
       this.likeNum++;
       this.isActive = true;
@@ -45,6 +88,9 @@ export default {
       // 애니메이션 클래스를 추가합니다.
       const buttonEl = this.$refs.myButton;
       buttonEl.classList.add("animateLike");
+
+      // likeNum 값을 로컬 스토리지에 저장
+      localStorage.setItem('likeNum', this.likeNum.toString());
     },
   },
 }
--- pf/src/components/WorkArea.vue
+++ pf/src/components/WorkArea.vue
@@ -1,13 +1,30 @@
 <template>
-  <div></div>
+   <div class="workAreaContent">
+    <div class="workAreaBxs">
+      <template v-for="(item, index) in workAreaBx">
+        <div class="workAreaBx" :key="index" v-if="id === null || item.id === id">
+          <div class="workAreaImg"><img :src="item.src" alt="workImg"/></div>
+          <div class="workAreaTxt">{{item.title}}</div>
+        </div>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'WorkArea',
-  props: {} ,
+  props: ['id'] ,
   data(){
     return{
+      workAreaBx: [
+        {src:'../img/mainBg.png',title: 'title01', id: 1},
+        {src:'../img/mainBg.png',title: 'title02', id: 1},
+        {src:'../img/mainBg.png',title: 'title03', id: 1},
+        {src:'../img/mainBg.png',title: 'title04', id: 2},
+        {src:'../img/mainBg.png',title: 'title05', id: 2},
+        {src:'../img/mainBg.png',title: 'title06', id: 3},
+      ],
     }
   },
   methods: {
