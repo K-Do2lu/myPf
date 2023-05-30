54caa95cd3b81233ebeafc35fecb678879eb975e
kdy <dy.kim@hkcloud.co.kr>
2023-05-30 17:46:16 +0900

    ohyeah

--- pf/src/components/ContactArea.vue
+++ pf/src/components/ContactArea.vue
@@ -1,18 +1,56 @@
 <template>
-  <div></div>
+  <div class="contactContent">
+    <div class="contactBx">
+      <div
+        class="contactTxt"
+        v-show="showContactTxt"
+        :class="{ active: showContactTxt && iconClicked }"
+      >
+        <span>잇쇼니 간바뗑 뿌뿌~🤪</span>
+      </div>
+      <div
+        class="contactIcon"
+        v-show="!showContactTxt"
+        @click="toggleContactTxt"
+      ></div>
+      <div class="contactInfo">
+        <span class="country">South Korea</span>
+        <span class="tell">+82 8686 7435</span>
+        <span class="email">kdy992da@naver.com</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ContactArea',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      showContactTxt: false,
+      iconClicked: false,
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  },
+  unmounted() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      const screenWidth = window.innerWidth;
+      this.showContactTxt = screenWidth >= 500;
+    },
+    toggleContactTxt() {
+      this.iconClicked = true;
+      this.showContactTxt = true;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 @import "../assets/sass/ContactArea.scss";
-
 </style>
--- pf/src/components/MainArea.vue
+++ pf/src/components/MainArea.vue
@@ -2,128 +2,135 @@
   <div class="mainContent">
     <video src="../assets/img/mainVideo.mp4" autoplay muted loop></video>
 
-   <div class="mainUserBx">
-    <div class="mainUser"></div>   
+    <div class="mainUserBx">
+      <div class="mainUser"></div>
 
-    <div class="mainUserTxtBx">
-      <span class="userName">do2lu</span>
-      <span class="userEmail">kdy992da@naver.com</span>
+      <div class="mainUserTxtBx">
+        <span class="userName">do2lu</span>
+        <span class="userEmail">kdy992da@naver.com</span>
 
-      <div class="mainUserInfos">
-        <div class="mainUserProfile" @click="openPopup">Profile</div>
-        <div class="mainUserCareer" @click="openPopup2">Career</div>
+        <div class="mainUserInfos">
+          <div class="mainUserProfile" @click="openPopup('aboutMe')">Profile</div>
+          <div class="mainUserCareer" @click="openPopup('career')">Career</div>
+          <div class="mainUserContact" @click="toggleContactArea">Call</div>
+        </div>
       </div>
     </div>
-   </div>
-
 
     <div class="mainBtnBx">
-
       <div class="mainBtns">
         <div class="mainBtn" @click="toggleBComponent(null)" :class="{ isActive: selectedId === null && selectedBtn }">All</div>
         <div class="mainBtn" @click="toggleBComponent(1)" :class="{ isActive: selectedId === 1 && selectedBtn }">UI/UX Design</div>
         <div class="mainBtn" @click="toggleBComponent(2)" :class="{ isActive: selectedId === 2 && selectedBtn }">Project</div>
         <div class="mainBtn" @click="toggleBComponent(3)" :class="{ isActive: selectedId === 3 && selectedBtn }">Work</div>
-        <button class="btn-secondary like-review"  @click="likeBtn">
-          <i class="fa fa-heart" aria-hidden="true" ref="myButton" :class="{ animateLike: isActive }"></i>{{likeNum}}
+        <button class="btn-secondary like-review" @click="likeBtn">
+          <i class="fa fa-heart" aria-hidden="true" ref="myButton" :class="{ animateLike: isActive }"></i>{{ likeNum }}
         </button>
       </div>
-
     </div>
-
-
   </div>
-<WorkArea v-if="show" ref="workArea" :id="selectedId"></WorkArea>
+
+  <WorkArea v-if="work" ref="workArea" :id="selectedId"></WorkArea>
+  <ContactArea v-if="contact" ref="contactArea" class="contact-area"></ContactArea>
 </template>
 
 <script>
 import WorkArea from '../components/WorkArea.vue';
+import ContactArea from '../components/ContactArea.vue';
 import pdfUrl from '../assets/img/aboutMe.pdf';
 import pdfUrl2 from '../assets/img/career.pdf';
 
 export default {
-name: 'MainArea',
-props: {
-
-},
-components: {
-  WorkArea,
-},
-data() {
-  return{
-    likeNum: 0,
-    isActive: false,
-    show: false,
-    selectedId: undefined,
-    selectedBtn: false,
-  }
-},
-created() {
-  // 페이지가 로드될 때, 로컬 스토리지에서 likeNum 값을 가져옴
-  const savedLikeNum = localStorage.getItem('likeNum');
-  if (savedLikeNum) {
-    this.likeNum = parseInt(savedLikeNum, 10);
-  }
-},
-methods: {
-  // workareashow
-  toggleBComponent(id) {
-    this.show = true;
-    this.selectedId = id;
-    this.selectedBtn = true;
-
-    this.$nextTick(() => {
-      if (this.show) {
-        this.scrollToWorkArea();
+  name: 'MainArea',
+  components: {
+    WorkArea,
+    ContactArea
+  },
+  data() {
+    return {
+      likeNum: 0,
+      isActive: false,
+      work: false,
+      contact: false,
+      selectedId: undefined,
+      selectedBtn: false,
+      pdfUrls: {
+        aboutMe: pdfUrl,
+        career: pdfUrl2
       }
-    });
+    };
   },
-  scrollToWorkArea() {
-    const workArea = this.$refs.workArea;
-    if (workArea) {
-      workArea.$el.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
+  created() {
+    const savedLikeNum = localStorage.getItem('likeNum');
+    if (savedLikeNum) {
+      this.likeNum = parseInt(savedLikeNum, 10);
+    }
+  },
+  methods: {
+    toggleBComponent(id) {
+      this.work = true;
+      this.selectedId = id;
+      this.selectedBtn = true;
+
+      this.$nextTick(() => {
+        if (this.work) {
+          this.scrollToWorkArea();
+        }
       });
-    }
-  },
-
-  //좋아요 버튼
-  likeBtn() {
-    this.likeNum++;
-    this.isActive = true;
-    
-    // 1초 후에 isActive를 false로 변경하고 애니메이션 클래스를 제거
-    setTimeout(() => {
-      this.isActive = false;
-      const buttonEl = this.$refs.myButton;
-      buttonEl.classList.remove("animateLike");
-    }, 500);
-
-    // 애니메이션 클래스를 추가
-    const buttonEl = this.$refs.myButton;
-    buttonEl.classList.add("animateLike");
-
-    // likeNum 값을 로컬 스토리지에 저장
-    localStorage.setItem('likeNum', this.likeNum.toString());
-    
-    //n단위로 alert창 표시
-    if(this.likeNum % 10 === 0){
-      alert('날 너무 좋아하네');
-    }
-  },
-  //openPopup
-  //추후 매개변수로 소스 수정 예정
-  openPopup() {
-      window.open(pdfUrl, '_blank', 'toolbar=no,scrollbars=yes,resizable=yes,top=100,left=100,width=600,height=800');
     },
-  openPopup2() {
-    window.open(pdfUrl2, '_blank', 'toolbar=no,scrollbars=yes,resizable=yes,top=100,left=100,width=600,height=800');
-  },
-    
+    scrollToWorkArea() {
+      const workArea = this.$refs.workArea;
+      if (workArea) {
+        workArea.$el.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    },
+    toggleContactArea() {
+      this.contact = !this.contact;
+      this.$nextTick(() => {
+        if (this.contact) {
+          this.scrollToContactArea();
+        }
+      });
+    },
+    scrollToContactArea() {
+      const contactArea = this.$refs.contactArea;
+      if (contactArea) {
+        contactArea.$el.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    },
+    likeBtn() {
+      this.likeNum++;
+      this.isActive = true;
 
-},
-}
+      setTimeout(() => {
+        this.isActive = false;
+        const buttonEl = this.$refs.myButton;
+        buttonEl.classList.remove('animateLike');
+      }, 500);
+
+      const buttonEl = this.$refs.myButton;
+      buttonEl.classList.add('animateLike');
+
+      localStorage.setItem('likeNum', this.likeNum.toString());
+
+      if (this.likeNum % 10 === 0) {
+        alert('5252~적당히 좋아하라구😏');
+      }
+    },
+    openPopup(key) {
+      const pdfUrl = this.pdfUrls[key];
+      if (pdfUrl) {
+        window.open(pdfUrl, '_blank', 'toolbar=no,scrollbars=yes,resizable=yes,top=100,left=100,width=600,height=800');
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
